7ebd5f75eef578c1471ab2b2d481d6187da6b067
meyby vicuna <meyby.vicuna@gmail.com>
2021-06-11 14:35:32 -0500

    Agregando api de productos, listado, por id y crear

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
-const userController = require('../../controller/v1/user-controller')
-
+const userController = require('../../controller/v1/user-controller');
+const productsController = require('../../controller/v1/product-controller');
 
 const createRoutesV1 = (app) => {
-    app.get('/api/v1/users', userController.getUsers);
-    app.get('/api/v1/users/:userId', userController.getUserById);
+  app.get('/api/v1/users', userController.getUsers);
+  app.get('/api/v1/users/:userId', userController.getUserById);
+  app.get('/api/v1/products', productsController.getProducts);
+  app.get('/api/v1/products/:productId', productsController.getProductById);
+  app.post('/api/v1/products/create', productsController.createProduct);
 };
 
 module.exports = createRoutesV1;
