ebcd65fdfc1d85a7d0850422bcfdda770b3e94bf
meyby vicuna <meyby.vicuna@gmail.com>
2021-07-02 00:14:20 -0500

    Agregando schema para products y creando su interface

---
+++ src/db/schemas/products.ts
@@ -0,0 +1,22 @@
+import { Schema, Document, model, ObjectId } from 'mongoose';
+import { User } from './users';
+
+interface Product extends Document {
+  name: string;
+  year: number;
+  price?: number;
+  description?: string;
+  user: ObjectId | User;
+}
+
+const schema = new Schema ({
+  name: { type: String, require: true },
+  year: { type: Number, require: true },
+  price: { type: Number, default: 0 },
+  description: String,
+  user: { type: Schema.Types.ObjectId, ref: 'user' } // Es como una llave foranea
+});
+
+const Products = model<Product>('product', schema);
+
+export default Products;
