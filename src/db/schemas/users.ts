a1299fedbf68137daf84331d0ce242cf583d4f3d
meyby vicuna <meyby.vicuna@gmail.com>
2021-07-02 00:13:28 -0500

    Agregando document y exportando interface de User

--- src/db/schemas/users.ts
+++ src/db/schemas/users.ts
@@ -1,6 +1,6 @@
-import { Schema, model } from 'mongoose';
+import { Schema, Document, model } from 'mongoose';
 
-interface User extends Document {
+export interface User extends Document {
   email: string;
   firstName: string;
   lastName: string;
