37e2ba0a88d7101d19a1a84a5dbce0858cce7edd
meyby vicuna <meyby.vicuna@gmail.com>
2021-08-14 00:43:07 -0500

    Actualizando las rutas para la conexión a una base de datos

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,19 +1,22 @@
 import { Application } from 'express';
 import * as userController from '../../controller/v1/user-controller';
-import *  as productsController from '../../controller/v1/product-controller';
+import * as productsController from '../../controller/v1/product-controller';
 
 const createRoutesV1 = (app: Application): void => {
-  // Migrando a conexion con base de datos en linea
   app.post('/api/v1/users/create', userController.createUser);
-  // Apis antiguas 
+  app.post('/api/v1/products/create', productsController.createProduct);
   app.get('/api/v1/users', userController.getUsers);
   app.get('/api/v1/users/:userId', userController.getUserById);
   app.get('/api/v1/products', productsController.getProducts);
   app.get('/api/v1/products/:productId', productsController.getProductById);
-  app.post('/api/v1/products/create', productsController.createProduct);
   app.put('/api/v1/products/:productId', productsController.updateProduct); // Actualizar toda la lista
-  app.patch('/api/v1/products/:productId', productsController.partialUpdateProduct); // Actualizar un campo de un item
-  app.post('/api/v1/products/:productId/notify-client', productsController.updateProductAndNotify);
-  app.delete('/api/v1/products/:productId', productsController.deleteProductById);
+  app.patch(
+    '/api/v1/products/:productId',
+    productsController.partialUpdateProduct
+  );
+  app.delete(
+    '/api/v1/products/:productId',
+    productsController.deleteProductById
+  );
 };
 export default createRoutesV1;
