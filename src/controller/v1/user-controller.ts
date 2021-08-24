c08530049def759189247362d71d1065e2e2fce0
meyby vicuna <meyby.vicuna@gmail.com>
2021-08-24 12:39:30 -0500

    Agregando Api de logeo donde se ingresa el email y el password

--- src/controller/v1/user-controller.ts
+++ src/controller/v1/user-controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
-import Users from '../../db/schemas/users';
+import Users, { User } from '../../db/schemas/users';
 import bcrypt from 'bcrypt';
 import { sendError, validaObjectId } from '../../utils/errors';
 import Products from '../../db/schemas/products';
+import jwt from 'jsonwebtoken';
 
 export const getUsers = async (rq: Request, rs: Response): Promise<void> => {
   const users = await Users.find().select({ password: 0, __v: 0 }); // Select sirve para que password: 0 no aparezca
@@ -43,21 +44,53 @@ export const createUser = async (rq: Request, rs: Response): Promise<void> => {
   }
 };
 
-export const deleteUserById = async (rq: Request, rs: Response): Promise<void> => {
+export const deleteUserById = async (
+  rq: Request,
+  rs: Response
+): Promise<void> => {
   try {
     const { userId } = rq.params;
     validaObjectId(userId);
     const user = await Users.findByIdAndDelete(userId);
 
     if (user) {
-      await Products.deleteMany({ user: user._id});
+      await Products.deleteMany({ user: user._id });
       rs.send('OK');
     } else {
       rs.send(404).send({});
     }
-
   } catch (e) {
     sendError(rs, e);
   }
-  
-}
+};
+
+export const login = async (rq: Request, rs: Response): Promise<void> => {
+  try {
+    const { email, password } = rq.body;
+    const user: User | null = await Users.findOne({ email });
+
+    if (!user) {
+      throw rs.status(404).send('User not found');
+    } else {
+      const isOk: boolean = await bcrypt.compare(password, user.password);
+
+      if (!isOk) {
+        throw rs.status(401).send('Invalid password');
+      }
+
+      const expiresIn = 60 * 60;
+      // Token
+      const token = jwt.sign(
+        { userId: user._id, email: user.email },
+        process.env.JWT_SECRET!,
+        {
+          expiresIn
+        }
+      );
+
+      rs.send({ toke: token, expiresIn });
+    }
+  } catch (e) {
+    sendError(rs, e);
+  }
+};
