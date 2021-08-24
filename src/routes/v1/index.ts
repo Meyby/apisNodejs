a2df29d33d8a769518951197f21807e8c671335b
meyby vicuna <meyby.vicuna@gmail.com>
2021-08-24 12:40:26 -0500

    Contenedor de las rutas y usuarios

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,24 +1,9 @@
 import { Application } from 'express';
-import * as userController from '../../controller/v1/user-controller';
-import * as productsController from '../../controller/v1/product-controller';
+import userRouter from './user-router';
+import productRouter from './product-router';
 
 const createRoutesV1 = (app: Application): void => {
-  app.post('/api/v1/users/create', userController.createUser);
-  app.post('/api/v1/products/create', productsController.createProduct);
-  app.get('/api/v1/users', userController.getUsers);
-  app.get('/api/v1/users/:userId', userController.getUserById);
-  app.delete('/api/v1/users/:userId', userController.deleteUserById);
-  app.get('/api/v1/products', productsController.getProducts);
-  app.get('/api/v1/products/:productId', productsController.getProductById);
-  app.put('/api/v1/products/:productId', productsController.updateProduct); // Actualizar toda la lista
-  app.patch(
-    '/api/v1/products/:productId',
-    productsController.partialUpdateProduct
-  );
-  app.delete(
-    '/api/v1/products/:productId',
-    productsController.deleteProductById
-  );
-  app.post('/api/v1/products/:productId/notify-client', productsController.updateProductAndNotify);
+  app.use('/api/v1/users', userRouter);
+  app.use('/api/v1/products', productRouter);
 };
 export default createRoutesV1;
