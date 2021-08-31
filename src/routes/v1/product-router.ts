c3b8852128742089dea722123ca097fddf9654e1
meyby vicuna <meyby.vicuna@gmail.com>
2021-08-30 22:26:28 -0500

    Agregando los middleware en las rutas

--- src/routes/v1/product-router.ts
+++ src/routes/v1/product-router.ts
@@ -1,18 +1,39 @@
 import { Router } from 'express';
 import * as productsController from '../../controller/v1/product-controller';
 import { checkAut } from '../../middlewares/auth-middleware';
+import { handleRequestErrors } from '../../middlewares/validator-moddleware';
+import {
+  validateDelete,
+  validateNewProductBody,
+  validateProductAndNotify,
+} from '../../validators/v1/products-validator';
 
 const router = Router();
 
 router.get('', checkAut, productsController.getProducts);
-router.post('/create', checkAut, productsController.createProduct);
+router.post(
+  '/create',
+  checkAut,
+  validateNewProductBody,
+  handleRequestErrors,
+  productsController.createProduct
+);
 router.get('', checkAut, productsController.getProducts);
 router.get('/:productId', checkAut, productsController.getProductById);
 router.put('/:productId', checkAut, productsController.updateProduct); // Actualizar toda la lista
 router.patch('/:productId', checkAut, productsController.partialUpdateProduct);
-router.delete('/:productId', checkAut, productsController.deleteProductById);
+router.delete(
+  '/:productId',
+  checkAut,
+  validateDelete,
+  handleRequestErrors,
+  productsController.deleteProductById
+);
 router.post(
-  '/:productId/notify-client', checkAut,
+  '/:productId/notify-client',
+  checkAut,
+  validateProductAndNotify,
+  handleRequestErrors,
   productsController.updateProductAndNotify
 );
 
